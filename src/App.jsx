c44de98cd1673c742197dea3ee9c64f0a30c35a2
Ushaim Ramadhan F <adhan1005@gmail.com>
2026-01-27 13:39:31 +0700

    create routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+const DetailPage = () => <p>Halaman Detail-tes</p>
+const AddPage = () => <p>Halaman Tambah-tes</p>
 
 function App() {
   return (
-    <div className="app-container">
-      <h1>Hello, React</h1>
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+        <header>
+          <h1><Link to='/'>Aplikasi Catatan</Link></h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/notes/new' element={<AddPage />} />
+            <Route path='/notes/:id' element={<DetailPage />} />
+          </Routes>
+        </main>
+      </div>    
+    </BrowserRouter>
   );
 }
 
